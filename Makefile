4e55bcc65eb3e403d97d6becd3925f7b7cb7d158
v9c9 <v9c9@ugrad.cs.ubc.ca>
2014-10-16 14:20:59 -0700

    UserCommits added for metior testing

---
+++ Makefile
@@ -0,0 +1,14 @@
+MAKE = make
+
+all: Pollock.jar
+
+Pollock.jar: Pollock.java
+	cd src/cleaner; $(MAKE)
+	cd src/synth; $(MAKE)
+
+clean:
+	cd src/cleaner; $(MAKE) clean
+	cd src/synth; $(MAKE) clean
+#going to have to download and install some ruby crap
+install:
+	
---
+++ UserCommits.rb
@@ -0,0 +1,9 @@
+# This code grabs a bunch of user information from github
+
+require 'metior'
+
+#Metior.report :git, '~/open-source/metior', './reports/metior'
+#Metior.report :github, 'koraktor/metior', './reports/rails'
+
+Metior.report :git, '~/School/410/rails', './reports/metior'
+#Metior.simple_stats :github, 'rails/rails', './reports/rails'
