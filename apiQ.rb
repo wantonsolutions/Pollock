bf04c280e5994b75da60884c7d99e6523ee429a7
v9c9 <v9c9@ugrad.cs.ubc.ca>
2014-10-26 23:46:39 -0700

    intial implementation of run.sh as a controlling script

--- apiQ.rb
+++ apiQ.rb
@@ -2,11 +2,12 @@
 
 require 'octokit'
 
+repository = 'StewartGrant/Pollock'
 output1 = File.open("UserCommits.txt","w")
 output2 = File.open("FileCommits.txt","w")
 
 #get user commits
-commits = Octokit.commits 'StewartGrant/Pollock'
+commits = Octokit.commits repository
 commits.each do|commit| 
 	output1 << commit.sha 
 	output1 << ","
@@ -15,10 +16,25 @@ commits.each do|commit|
 end
 output1.close
 
+#commits.each do|commit|
+#	files = commit.files
+#	puts commit.files
+#	files.each do|file|
+#		output2 << commit.sha
+#		output2 << ","
+#		output2 << file.filename
+#		output2 << ","
+#		output2 << file.additions
+#		output2 << ","
+#		output2 << file.deletions
+#		output2 << ","
+#	end
+#end
+		
+	
+
 #get File Commits
-
-
-
-
-
-
+#filename
+#sha
+#additions
+#deletions
--- run.sh
+++ run.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+args=("$@")
+repo=${args[0]}
+ruby apiQ.rb
+#make a repo to clone into
+mkdir tempDir
+cd tempDir
+git clone $repo
+repoName=`ls`
+cd ..
+echo $repoName
+
+mkdir sgout
+gitstats tempDir/$repoName ./sgout
+
+ant run
+
+#this line will have to take the output of pollock and throw it at processing
+
+#yes | rm -r sgout
+#yes | rm -r tempDir
+
