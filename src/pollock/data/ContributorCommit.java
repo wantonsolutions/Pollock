6423a4e91ae0c92a17e8e955439129ed5f80d14b
Anastasiya <anastasiya.ger@gmail.com>
2014-11-08 20:58:39 -0800

    ContributorCommit Class changes
    fields filename and date removed

--- src/pollock/data/ContributorCommit.java
+++ src/pollock/data/ContributorCommit.java
@@ -5,15 +5,15 @@ public class ContributorCommit
 {
 	private String contributor;
 	private String commit;
-	private String date;
-	private String filename;
+	//private String date;
+	//private String filename;
 
-	public ContributorCommit(String contributor, String commit, String date, String filename)
+	public ContributorCommit(String contributor, String commit)
 	{
 		this.contributor = contributor;
 		this.commit = commit;
-		this.date = date;
-		this.filename = filename;
+		//this.date = date;
+		//this.filename = filename;
 	}
 	
 }
