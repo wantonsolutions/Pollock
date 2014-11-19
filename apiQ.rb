51eec33b65ae1106c2f05e3368cecb45d48120f8
wontonsolutions <stewbertgrant@gmail.com>
2014-11-19 08:44:46 -0800

    almost final update of executables

--- apiQ.rb
+++ apiQ.rb
@@ -1,8 +1,6 @@
 # This code grabs a bunch of user information from github
 
 require 'octokit'
-require 'thread/pool'
-require 'thread'
 
 usage = "Usage: \truby apiQ.rb <repo name> <github username> <github password>\nex)\truby apiQ.rb wantonsolutions/Pollock username password\n\n"
 
@@ -20,68 +18,56 @@ end
 #setup user login
 if ARGV.length == 3
 	puts "logging in"
-	@client = Octokit::Client.new \
+	client = Octokit::Client.new \
 		:login =>ARGV[1],
 		:password =>ARGV[2]
-	user = @client.user
+	user = client.user
 	user.login
 else
 	puts "Insufficiant login info using limited requester"
 	puts usage
-	@client = Octokit
+	client = Octokit
 end
 
 
-@output1 = File.open("UserCommits.txt","w")
-@output2 = File.open("FileCommits.txt","w")
+output1 = File.open("UserCommits.txt","w")
+output2 = File.open("FileCommits.txt","w")
 
-@lockf1 = Mutex.new
-@lockf2 = Mutex.new
 sha = 0 
 #get user commits
 
 threads = []
 #gets #otherwise the program ends without doing anything
 
-@client.auto_paginate = true;
-commits = @client.commits repository
-pool = Thread.pool(commits.length)
+client.auto_paginate = true;
+commits = client.commits repository
 commits.each do|commit| 
-	pool.process{
 	puts commit.commit.committer.date, commit.sha, "\n"
-	#@lockf1.synchronize{
-	@output1 << commit.sha 
-	@output1 << ","
-	@output1 << commit.commit.committer.name
-	@output1 << ","
-	@output1 << commit.commit.committer.date
-	@output1 << "\n"
-	#}
+	output1 << commit.sha 
+	output1 << ","
+	output1 << commit.commit.committer.name
+	output1 << ","
+	output1 << commit.commit.committer.date
+	output1 << "\n"
 	
-	puts "infile1"
-	#@lockf2.synchronize{
-	icommit = @client.commit(repository, commit.sha)
+	icommit = client.commit(repository, commit.sha)
 	files = icommit.files
 	files.each do|file|
-		puts icommit.sha
-		@output2 << icommit.sha
-		@output2 << ","
-		@output2 << file.filename
-		@output2 << ","
-		@output2 << file.additions
-		@output2 << ","
-		@output2 << file.deletions
-		@output2 << "\n"
+		output2 << icommit.sha
+		output2 << ","
+		output2 << file.filename
+		output2 << ","
+		output2 << file.additions
+		output2 << ","
+		output2 << file.deletions
+		output2 << "\n"
 	end
-	@output2 << "\n"
-	#}
+	output2 << "\n"
 	sleep(1);
-}
 
-	pool.shutdown
 end
-@output1.close
-@output2.close
+output1.close
+output2.close
 		
 	
 
--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ args=("$@")
 repo=${args[0]}
 uname=${args[1]}
 pword=${args[2]}
-pDir="../processing-2.2.1"
+pDir=${args[3]}
 
-tempdir=`fgrep pdir .conf`
+#pDir="../processing-2.2.1"
+
+#tempdir=`fgrep pdir .conf`
 
 #OIFS="$IFS"
 #IFS=':' 
@@ -22,16 +24,20 @@ ruby apiQ.rb $repo $uname $pword
 
 ant run
 
+tac FileCommits.txt > FileCommits.txt
+tac UserCommits.txt > UserCommits.txt
+
 #move the drawable output to processing
 mv out.txt $pDir/draw/draw/data
 cp src/pollock/draw.pde $pDir/draw/draw/
+cur=`pwd`
 cd $pDir
 ./processing-java --run --force --sketch=draw/draw --output=r
 
 #this line will have to take the output of pollock and throw it at processing
 #sleep 10
 
-cd ../pollock
+cd $cur
 
 ant distclean
 yes | rm FileCommits.txt
